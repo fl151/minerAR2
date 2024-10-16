e8715bf6e0ecd6fa803a9ecd40d49a05b7fbb9ae
FL151 <fl151uze@gmail.com>
2024-10-16 21:10:09 +0700

    Added player view for lose and win

--- Assets/Scripts/Player/View/PlayerModelStateMachine.cs
+++ Assets/Scripts/Player/View/PlayerModelStateMachine.cs
@@ -6,8 +6,11 @@ public class PlayerModelStateMachine : MonoBehaviour
     [Space]
     [SerializeField] private PlayerModelView _player;
     [SerializeField] private FlagModelView _flag;
+    [SerializeField] private DeadPlayerModelView _dead;
+    [SerializeField] private WinPlayerViewModel _win;
 
     private ModelView _currentModelView;
+    private bool _isGameStoped = false;
 
     private void Awake()
     {
@@ -17,30 +20,66 @@ public class PlayerModelStateMachine : MonoBehaviour
     private void OnEnable()
     {
         _flagButton.OnClicked += OnButtonClicked;
+
+        GameResultController.Instance.Win += OnWin;
+        GameResultController.Instance.Lose += OnLose;
+        GameResultController.Instance.Restarted += OnRestart;
         _currentModelView.Activate();
     }
 
     private void OnDisable()
     {
+        GameResultController.Instance.Win -= OnWin;
+        GameResultController.Instance.Lose -= OnLose;
+        GameResultController.Instance.Restarted -= OnRestart;
+
         _flagButton.OnClicked -= OnButtonClicked;
         _currentModelView.Deactivate();
     }
 
     private void OnButtonClicked()
     {
-        if (_flagButton.IsActive)
+        if(_isGameStoped == false)
         {
-            _player.Deactivate();
-            _flag.Activate();
+            _currentModelView.Deactivate();
 
-            _currentModelView = _flag;
-        }
-        else
-        {
-            _flag.Deactivate();
-            _player.Activate();
+            if (_flagButton.IsActive) 
+            { 
+                _flag.Activate();
+                _currentModelView = _flag;
+            }
+            else
+            {
+                _player.Activate();
+                _currentModelView = _player;
+            }
+        } 
+    }
 
-            _currentModelView = _player;
-        }
+    private void OnWin()
+    {
+        OnGameStoped(_win);
+    }
+
+    private void OnLose()
+    {
+        OnGameStoped(_dead);
+    }
+
+    private void OnRestart()
+    {
+        _isGameStoped = false;
+
+        OnButtonClicked();
+    }
+
+    private void OnGameStoped(ModelView result)
+    {
+        _isGameStoped = true;
+
+        _currentModelView.Deactivate();
+        result.Activate();
+
+        _currentModelView = result;
     }
 }
